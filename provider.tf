600be1cd771056672bbe92326b6dc5c98b080640
vijaysony328-lgtm <vijaysony328@gmail.com>
2026-09-08 13:24:27 +0530

    update

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@
 terraform {
     required_providers {
         azurerm ={
-            source ="hashicorp/azurem"
+            source ="hashicorp/azurerm"
             version =">=4.0.0"
         }
     }
